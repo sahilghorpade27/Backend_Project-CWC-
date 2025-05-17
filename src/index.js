d8a869a3cb2ad5d1a822a884b99684e0fa309871
Sahil Ghorpade <sahilpg27@gmail.com>
2025-05-17 22:38:56 +0530

    util file updation and API handling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB()
+.then(() =>{
+app.listen(process.env.PORT || 8000     , () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    app.on("errror",(error)=>{
+            console.log("ERRR : ",error)
+            throw error ;
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connectioon FAILED !!!",err);
+})
 
 
 
